74424412faa29eae3759f70c8ee4f75fe351bcb4
Chris Halogen <christackoms@gmail.com>
2023-10-11 01:58:18 +0100

    Header & Footer Complete

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 if (document.getElementById("mobile-menu-toggle")) {
   let child = document.getElementById("mobile-menu-toggle");
   let parent = child.parentElement;
+  let close_icon = document.getElementById("close-icon-icon");
+  let panel = document.getElementById("mobile-menu-panel");
 
-  child.addEventListener("click", function () {
+  child.addEventListener("click", toggleMenu);
+  close_icon.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
     parent.classList.toggle("active");
-  });
+    panel.classList.toggle("open");
+  }
 
   let parent_menu = document.querySelector(
     ".mobile-menu nav > ul > li > a:not(:only-child)"
@@ -13,7 +19,13 @@ if (document.getElementById("mobile-menu-toggle")) {
   //   let sub_menu = parent_menu.querySelector("a:not(:only-child) + ul");
   parent_menu.addEventListener("click", function () {
     parent_menu.nextElementSibling.classList.toggle("open");
-    console.log("ready");
+
+    if (parent_menu.nextElementSibling.classList.contains("open")) {
+      parent_menu.nextElementSibling.style.maxHeight =
+        parent_menu.nextElementSibling.scrollHeight + "px";
+    } else {
+      parent_menu.nextElementSibling.style.maxHeight = "0";
+    }
   });
   //   console.log(sub_menu);
 }
